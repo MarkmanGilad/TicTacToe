dbf19713fa52a440e881bce15f23319d4aabf240
Gilad Markman <gilad@markman.co.il>
2026-03-08 11:37:39 +0100

    update

--- Image.py
+++ Image.py
@@ -6,28 +6,31 @@ pygame.init()
 
 def main ():
 
-
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption('Images')
     clock = pygame.time.Clock()
 
     screen.fill('LIGHTGRAY')
+
     img = pygame.image.load("img/space_ship.png")
     img = pygame.transform.scale(img, (50, 50))
 
-    x, y = 50, 50
-    screen.blit(img, (x, y))
-
+    x, y = 0, 0
+    vx, vy = 2, 0
+    
     run = True
     while (run):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                run = False
-            
-        x += 1
 
+        screen.fill('LIGHTGRAY') 
         screen.blit(img, (x, y))
-
+        # תרגיל א
+        x = x + 1
+        y = y + 2
+        # תרגיל ב
+        
         pygame.display.update()
         clock.tick(60)
     
